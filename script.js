21f2509c79bcb95d1fbf88b00cd6cec6d7753d1f
fayazfayyu862-beep <fayazfayyu862@gmail.com>
2026-08-03 22:44:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,28 @@ document.getElementById("openBtn").addEventListener("click",()=>{
     alert("Chapter 2 is coming... ❤️");
 
 });
+const hearts = document.getElementById("hearts");
+
+function createHeart(){
+
+    const heart = document.createElement("div");
+
+    heart.innerHTML = "❤";
+
+    heart.classList.add("heart");
+
+    heart.style.left = Math.random()*100+"vw";
+
+    heart.style.animationDuration = (4+Math.random()*3)+"s";
+
+    hearts.appendChild(heart);
+
+    setTimeout(()=>{
+
+        heart.remove();
+
+    },7000);
+
+}
+
+setInterval(createHeart,500);
