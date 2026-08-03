76d7c66fcfc1e16e0a428ad2b90306ded3f0ecf2
fayazfayyu862-beep <fayazfayyu862@gmail.com>
2026-08-03 21:57:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector("button");
+
+    if (button) {
+        button.addEventListener("click", () => {
+            alert("Hi! I'm Baby 💖\nI'm 8 years old.\nLet's talk together! 🌸");
+        });
+    }
+});
